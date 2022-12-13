9494c08486a0fb4da5ab6a0cd3167b7d47d97823
Haniel Mikhaiel <Hanielmikhaiel@icloud.com>
2022-12-13 13:00:33 -0800

    Update Spot_Proxy.js

--- Spot_Proxy.js
+++ Spot_Proxy.js
@@ -5,5 +5,5 @@ url = searchparam.get("RAW");
 if(searchparam.has('RAW') && confirm('"' + url + '" is blocked for "'+ searchparam.get("CAT") + '"\nWould you like to unblock using ultraviolet?')){
   (function(){
     "http" != url.substring(0, 4) && (url = "https://" + url);
-    winloc.href = "https://reallypurple-node-production.up.railway.app/uv/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();
+    winloc.href = "https://spotavioletproxy.up.railway.app/uv/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();
 }
