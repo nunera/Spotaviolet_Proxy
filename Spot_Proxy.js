bd61d76fcf8213d9ea48f609e2a990e60f73a053
Haniel Mikhaiel <hanielmikhaiel@spotech.dev>
2023-01-15 20:44:12 -0800

    Update Spot_Proxy.js

--- Spot_Proxy.js
+++ Spot_Proxy.js
@@ -8,7 +8,7 @@ url = searchparam.get("RAW");
 if(searchparam.has('RAW') && confirm('"' + url + '" is blocked for "'+ searchparam.get("CAT") + '"\nWould you like to unblock using ultraviolet?')){
   (function(){
     "http" != url.substring(0, 4) && (url = "https://" + url);
-    winloc.href = "https://spotavioletproxy.up.railway.app/uv/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();
+    winloc.href = "https://proxy.spotech.dev/uv/service/" + encodeURIComponent(url.toString().split("").map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(""));})();
 }
 
 /// Eurda.js
