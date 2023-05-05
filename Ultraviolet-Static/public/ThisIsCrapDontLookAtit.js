41da247c24292d7504158b055bf89cc08c110a25
Haniel Mikhaiel <hanielmikhaiel@spotech.dev>
2023-05-05 22:33:11 +0000

    added encryption lol

---
+++ Ultraviolet-Static/public/ThisIsCrapDontLookAtit.js
@@ -0,0 +1,7 @@
+const CryptoJS = require('crypto-js');
+const encrypt = (text) => {
+  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(text));
+};
+const decrypt = (data) => {
+  return CryptoJS.enc.Base64.parse(data).toString(CryptoJS.enc.Utf8);
+};
